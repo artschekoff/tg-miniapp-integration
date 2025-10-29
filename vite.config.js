53e70d6c271e51b8614dfb90c5d689b008db5b43
Krivoshchekov Artem <therawlex@gmail.com>
2025-10-28 21:41:48 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ['localdev.roxl.net'],
+    allowedHosts: ['localdev.roxl.net', 'app.playdino.fun'],
     host: '0.0.0.0',
     port: 5173,
   },
